7213c1b662c4e83290f319dab00270314915b01d
Matthew Delves <matt@reformedsoftware.com>
2015-01-20 12:38:29 +1100

    Don't use xcpretty

--- build_and_test.sh
+++ build_and_test.sh
@@ -31,9 +31,4 @@ git checkout -qf "$BUILDBOX_COMMIT"
 git submodule init
 git submodule update
 
-# Remove DerivedData and Caches
-rm -Rf ~/Library/Caches/com.apple.dt.Xcode
-rm -Rf ~/Library/Caches/com.apple.dt.XcodeDeviceMonitor
-rm -Rf ~/Library/Developer/Xcode/DerivedData
-
-set -o pipefail && xcrun xcodebuild -workspace BuildkiteKit.xcworkspace -scheme BuildkiteKit -sdk iphonesimulator8.2 -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.2' clean build test | xcpretty -c
+set -o pipefail && xcrun xcodebuild -workspace BuildkiteKit.xcworkspace -scheme BuildkiteKit -sdk iphonesimulator8.2 -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.2' clean build test
