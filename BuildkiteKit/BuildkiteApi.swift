c5917854e323a098cff5bc2775ae442a0c5528de
Matthew Delves <matt@reformedsoftware.com>
2015-01-29 22:26:49 +1100

    Simplify JSON parsing, error handling and looping

--- BuildkiteKit/BuildkiteApi.swift
+++ BuildkiteKit/BuildkiteApi.swift
@@ -21,138 +21,120 @@ public class BuildkiteApi {
   public init (_ apiKey: String, scheme: String = "https", configuration: NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()) {
     self.apiKey = apiKey
     self.scheme = scheme
-
     self.session = NSURLSession(configuration: configuration)
   }
   
   public func getOrganizations(completion: (organizations: [Organization], body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.Organizations, apiKey, scheme: scheme)
-    
-    ArrayOfJSONDataForEndpoint(url) { json, data, response, error in
+    JSONDataForEndpoint(url) { json, data, response, error in
       var organizations: [Organization] = [Organization]()
-      
-      if let error : BuildkiteApiError = error {
-        completion(organizations: organizations, body: data, response: response, error: error)
-      } else {
-        for accountData:[String: AnyObject] in json! as [[String: AnyObject]]{
-          organizations.append(Organization(accountData))
+      if let json = json as? [[String: AnyObject]] {
+        if error == nil {
+          organizations = json.map { organization in
+            Organization(organization)
+          }
         }
-        
-        completion(organizations: organizations, body: data, response: response, error: nil)
       }
+      completion(organizations: organizations, body: data, response: response, error: error)
     }
   }
   
   public func getOrganization(name: String, completion: (organization: Organization?, body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.Organization(organization: name), apiKey, scheme: scheme)
-    
     JSONDataForEndpoint(url) { json, data, response, error in
       var organization : Organization?
-      
-      if error == nil {
-        organization = Organization(json! as [String: AnyObject])
+      if let json = json as? [String: AnyObject] {
+        if error == nil {
+          organization = Organization(json)
+        }
       }
-      
       completion(organization: organization, body: data, response: response, error: error)
     }
   }
   
   public func getProjects(account: String, completion: (projects: [Project], body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.Projects(organization: account), apiKey, scheme: scheme)
-    ArrayOfJSONDataForEndpoint(url) { json, data, response, error in
+    JSONDataForEndpoint(url) { json, data, response, error in
       var projects: [Project] = [Project]()
-      
-      if let error : BuildkiteApiError = error {
-        completion(projects: projects, body: data, response: response, error: error)
-      } else {
-        for projectData: [String: AnyObject] in json! as [[String: AnyObject]]{
-          projects.append(Project(projectData))
+      if let json = json as? [[String: AnyObject]] {
+        if error == nil {
+          projects = json.map { project in
+            Project(project)
+          }
         }
-        
-        completion(projects: projects, body: data, response: response, error: nil)
       }
+      completion(projects: projects, body: data, response: response, error: error)
     }
   }
   
   public func getProject(account: String, projectName: String, completion: (project: Project?, body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.Project(organization: account, project: projectName), apiKey, scheme: scheme)
-    
     JSONDataForEndpoint(url) { json, data, response, error in
       var project : Project?
-      
-      if error == nil {
-        project = Project(json! as [String: AnyObject])
+      if let json = json as? [String: AnyObject] {
+        if error == nil {
+          project = Project(json)
+        }
       }
-      
       completion(project: project, body: data, response: response, error: error)
     }
   }
   
   public func getBuilds(completion: (builds: [Build], body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.AllBuilds, apiKey, scheme: scheme)
-    
-    ArrayOfJSONDataForEndpoint(url) { jsonArray, data, response, error in
+    JSONDataForEndpoint(url) { json, data, response, error in
       var builds: [Build] = [Build]()
-
-      if let error : BuildkiteApiError = error {
-        completion(builds: builds, body: data, response: response, error: error)
-      } else {
-        for buildData : [String: AnyObject] in jsonArray! as [[String: AnyObject]] {
-          builds.append(Build(buildData))
+      if let json = json as? [[String: AnyObject]] {
+        if error == nil {
+          builds = json.map { build in
+            Build(build)
+          }
         }
-        
-        completion(builds: builds, body: data, response: response, error: nil)
       }
+      completion(builds: builds, body: data, response: response, error: error)
     }
   }
   
   public func getBuilds(account: String, projectName: String, completion: (builds: [Build], body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.Builds(organization: account, project: projectName), apiKey, scheme: scheme)
-    
-    ArrayOfJSONDataForEndpoint(url) { jsonArray, data, response, error in
+    JSONDataForEndpoint(url) { json, data, response, error in
       var builds: [Build] = [Build]()
-      
-      if let error : BuildkiteApiError = error {
-        completion(builds: builds, body: data, response: response, error: error)
-      } else {
-        for buildData : [String: AnyObject] in jsonArray! as [[String: AnyObject]] {
-          builds.append(Build(buildData))
+      if let json = json as? [[String: AnyObject]] {
+        if error == nil {
+          builds = json.map { build in
+            Build(build)
+          }
         }
-        
-        completion(builds: builds, body: data, response: response, error: nil)
       }
+      completion(builds: builds, body: data, response: response, error: error)
     }
   }
   
   public func getBuild(account: String, projectName: String, number: Int, completion: (build: Build?, body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.Build(organization: account, project: projectName, build: number), apiKey, scheme: scheme)
-    
     JSONDataForEndpoint(url) { json, data, response, error in
-      var build : Build?
-      
-      if error == nil {
-        build = Build(json! as [String: AnyObject])
+      var build: Build?
+      if let json = json as? [String: AnyObject] {
+        if error == nil {
+          build = Build(json)
+        }
       }
-      
       completion(build: build, body: data, response: response, error: error)
     }
   }
   
   public func getAgents(account: String, completion: (agents: [Agent], body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.Agents(organization: account), apiKey, scheme: scheme)
-    
-    ArrayOfJSONDataForEndpoint(url) { jsonArray, data, response, error in
+    JSONDataForEndpoint(url) { json, data, response, error in
       var agents: [Agent] = [Agent]()
-      
-      if let error : BuildkiteApiError = error {
-        completion(agents: agents, body: data, response: response, error: error)
-      } else {
-        for agentData: [String: AnyObject] in jsonArray! as [[String: AnyObject]] {
-          agents.append(Agent(agentData))
+      if let json = json as? [[String: AnyObject]] {
+        if error == nil {
+          agents = json.map { agent in
+            Agent(agent)
+          }
         }
-        
-        completion(agents: agents, body: data, response: response, error: nil)
       }
+      completion(agents: agents, body: data, response: response, error: error)
     }
   }
   
@@ -176,68 +158,37 @@ public class BuildkiteApi {
 
     JSONDataForRequest(request) { json, data, response, error in
       var token: AccessToken?
-
-      if error == nil {
-        token = AccessToken(json! as [String: AnyObject])
+      if let json = json as? [String: AnyObject] {
+        if error == nil {
+          token = AccessToken(json)
+        }
       }
-
       completion(token: token, body: data, response: response, error: error)
     }
   }
 
   public func getUser(completion: (user: User?, body: NSData?, response: NSHTTPURLResponse?, error: BuildkiteApiError?) -> Void) {
     let url = buildkiteEndpoint(BuildkiteURL.User, apiKey, scheme: scheme)
-    
     JSONDataForEndpoint(url) { json, data, response, error in
       var user : User?
-      
-      if error == nil {
-        user = User(json! as [String: AnyObject])
+      if let json = json as? [String: AnyObject] {
+        if error == nil {
+          user = User(json)
+        }
       }
-      
       completion(user: user, body: data, response: response, error: error)
     }
   }
-  
-  func ArrayOfJSONDataForEndpoint(url: NSURL, completion: ([[String: AnyObject]]?, NSData?, NSHTTPURLResponse?, BuildkiteApiError?) -> Void) {
-    let task = session.dataTaskWithURL(url) { data, response, error in
-      if let theResponse : NSHTTPURLResponse = response as? NSHTTPURLResponse {
-        let code = theResponse.statusCode
-        var error : BuildkiteApiError?
-        var nserror: NSError?
-        var jsonArray: [[String: AnyObject]]?
-        
-        if(code != 200) {
-          var body : [String: AnyObject] = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &nserror) as [String: AnyObject]
-          error = BuildkiteApiError(code: code, reason: body["message"] as String)
-        } else {
-          // Need to be able to distinguish between array and dict returns
-          jsonArray = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &nserror) as [[String: AnyObject]]?
-        }
-        
-        completion(jsonArray, data, theResponse, error)
-      }
-    }
-    
-    task.resume()
-  }
-  
-  func JSONDataForRequest(request: NSURLRequest, completion: ([String: AnyObject]?, NSData?, NSHTTPURLResponse?, BuildkiteApiError?) -> Void) {
+
+  func JSONDataForRequest(request: NSURLRequest, completion: (AnyObject?, NSData?, NSHTTPURLResponse?, BuildkiteApiError?) -> Void) {
     let task = session.dataTaskWithRequest(request) { data, response, error in
       if let theResponse: NSHTTPURLResponse = response as? NSHTTPURLResponse {
         let code = theResponse.statusCode
-        var error: BuildkiteApiError?
-        var nserror: NSError?
-        var json: [String: AnyObject]? = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &nserror) as [String: AnyObject]?
-
-        if (code != 200) {
-          if let json = json {
-            error = BuildkiteApiError(code: code, reason: json["message"] as String)
-          } else {
-            error = BuildkiteApiError(code: code, reason: "unknown error")
-          }
+        var error : BuildkiteApiError?
+        var json: AnyObject? = self.extractJSON(data)
+        if(code != 200) {
+          error = self.extractError(json, code: code)
         }
-
         completion(json, data, theResponse, error)
       }
     }
@@ -245,26 +196,32 @@ public class BuildkiteApi {
     task.resume()
   }
 
-  func JSONDataForEndpoint(url: NSURL, completion: ([String: AnyObject]?, NSData?, NSHTTPURLResponse?, BuildkiteApiError?) -> Void) {
+  func JSONDataForEndpoint(url: NSURL, completion: (AnyObject?, NSData?, NSHTTPURLResponse?, BuildkiteApiError?) -> Void) {
     let task = session.dataTaskWithURL(url) { data, response, error in
       if let theResponse : NSHTTPURLResponse = response as? NSHTTPURLResponse {
         let code = theResponse.statusCode
         var error : BuildkiteApiError?
-        var nserror : NSError?
-        var json : [String: AnyObject]?
+        var json: AnyObject? = self.extractJSON(data)
         if(code != 200) {
-          var body : [String: AnyObject] = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &nserror) as [String: AnyObject]
-          error = BuildkiteApiError(code: code, reason: body["message"] as String)
-        } else {
-          // Need to be able to distinguish between array and dict returns
-          json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &nserror) as [String: AnyObject]?
+          error = self.extractError(json, code: code)
         }
-        
         completion(json, data, theResponse, error)
       }
     }
-    
     task.resume()
   }
 
+  func extractError(json: AnyObject?, code: Int) -> BuildkiteApiError? {
+    var error: BuildkiteApiError?
+    if let json = json as? [String: AnyObject] {
+      error = BuildkiteApiError(code: code, reason: json["message"] as String)
+    }
+    return error
+  }
+
+  func extractJSON(data: NSData) -> AnyObject? {
+    var nserror: NSError?
+    var json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &nserror)
+    return json
+  }
 }
