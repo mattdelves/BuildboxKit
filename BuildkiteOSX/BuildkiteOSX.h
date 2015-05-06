b7e57f7519e8372687bc73adc2667ae1d57ce340
Matt Delves <matt.delves@outware.com.au>
2015-05-06 19:43:27 +1000

    Add in OSX target

---
+++ BuildkiteOSX/BuildkiteOSX.h
@@ -0,0 +1,19 @@
+//
+//  BuildkiteOSX.h
+//  BuildkiteOSX
+//
+//  Created by Matt Delves on 6/05/2015.
+//  Copyright (c) 2015 Matthew Delves. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BuildkiteOSX.
+FOUNDATION_EXPORT double BuildkiteOSXVersionNumber;
+
+//! Project version string for BuildkiteOSX.
+FOUNDATION_EXPORT const unsigned char BuildkiteOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuildkiteOSX/PublicHeader.h>
+
+
